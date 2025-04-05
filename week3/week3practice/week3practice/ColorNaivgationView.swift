0e0c1b24dcaf44f3cd3a3769864f975a8a758aa1
alwn8918 <alwn8918@gmail.com>
2025-04-06 06:20:01 +0900

    [week3] 실습: View 만들기

--- week3/week3practice/week3practice/ColorNaivgationView.swift
+++ week3/week3practice/week3practice/ColorNaivgationView.swift
@@ -6,13 +6,34 @@
 //
 
 import SwiftUI
+import Observation
 
 struct ColorNaivgationView: View {
+    @Bindable var viewModel: RainbowViewModel
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 70) {
+            if let selectedRainbowModel = viewModel.selectedRainbowModel {
+                Text("현재 선택된 색상 \n\(selectedRainbowModel.returnColorName())")
+                    .font(.title)
+                    .foregroundStyle(.black)
+                
+                Button(action: {
+                    viewModel.appleLogoColor = selectedRainbowModel.returnColor()
+                    dismiss()
+                }, label: {
+                    Text("사과 색 바꾸기")
+                        .padding(.vertical, 43)
+                        .padding(.horizontal, 36)
+                        .overlay(content: {
+                            RoundedRectangle(cornerRadius: 30)
+                                .fill(.clear)
+                                .stroke(.black, style: .init(lineWidth: 1))
+                        })
+                })
+            }
+        }
+        .navigationTitle("색 네비")
     }
-}
-
-#Preview {
-    ColorNaivgationView()
 }
--- week3/week3practice/week3practice/RainbowView.swift
+++ week3/week3practice/week3practice/RainbowView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct RainbowView: View {
+    @State private var navigationTrue: Bool = false
+    
+    var viewModel: RainbowViewModel = .init()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            NavigationStack {
+                VStack {
+                    colorCardGroup
+                    
+                    Spacer()
+                    
+                    bottomSelectedColorGroup
+                }
+                .safeAreaPadding(EdgeInsets(top: 47, leading: 15, bottom: 43, trailing: 15))
+                .navigationDestination(isPresented: $navigationTrue, destination: {
+                    ColorNaivgationView(viewModel: viewModel)
+                })
+            }
+        }
+    
+    // MARK: - group
+    private var colorCardGroup: some View {
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 40), count: 3), spacing: 26, content: {
+            ForEach(RainbowModel.allCases, id: \.self, content: { rainbow in
+                makeColorCard(rainbow)
+                    .onTapGesture {
+                        viewModel.selectedRainbowModel = rainbow
+                        self.navigationTrue.toggle()
+                    }
+            })
+        })
+    }
+    
+    private var bottomSelectedColorGroup: some View {
+        VStack(spacing: 70, content: {
+            Image(.logoApple)
+                .fixedSize()
+                .foregroundStyle(viewModel.appleLogoColor ?? .black)
+            
+            Text(selectedColorName())
+                .font(.title)
+                .foregroundStyle(.black)
+        })
+    }
+    
+    // MARK: - fucntion
+    private func makeColorCard(_ model: RainbowModel) -> some View {
+        VStack(spacing: 6, content: {
+            Rectangle()
+                .fill(model.returnColor())
+            
+            Text(model.returnColorName())
+                .foregroundStyle(.black)
+                .font(.title)
+        })
+        .frame(maxWidth: .infinity, minHeight: 110)
+    }
+    
+    private func selectedColorName() -> String {
+        if let name = viewModel.selectedRainbowModel {
+            return name.returnColorName()
+        } else {
+            return "아무것도 없음"
+        }
     }
 }
 

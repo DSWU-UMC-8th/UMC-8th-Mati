10ab6cb0c78465b9210e1a3ac154b438980aff4d
alwn8918 <alwn8918@gmail.com>
2025-04-13 03:35:42 +0900

    [week4] 실습: Model 작성하기

--- week4/week4practice/week4practice/ReceiptsModel.swift
+++ week4/week4practice/week4practice/ReceiptsModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class ReceiptsModel {
+    @Attribute(.unique) var id: UUID
+    
+    var orderer: String
+    var store: String
+    var menuItems: [String]
+    var totalAmount: Int
+    var orderNumber: String
+    var createAt: Date
+    
+    init(
+        orderer: String,
+        store: String,
+        menuItems: [String],
+        totalAmount: Int,
+        orderNumber: String,
+        createAt: Date = Date()
+    ) {
+        self.id = UUID()
+        self.orderer = orderer
+        self.store = store
+        self.menuItems = menuItems
+        self.totalAmount = totalAmount
+        self.orderNumber = orderNumber
+        self.createAt = createAt
+    }
+}

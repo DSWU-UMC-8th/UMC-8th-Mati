0eadb9cd9d91a5f2d8fc91ac88054be92f75a8a7
alwn8918 <alwn8918@gmail.com>
2025-05-12 02:34:31 +0900

    [week5] 실습

---
+++ week5/week5practice/week5practice/Model/MyModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyModel.swift
+//  week5practice
+//
+//  Created by 김미주 on 5/6/25.
+//
+
+import Foundation
+
+struct MyModel: Codable {
+    let nickname: String
+    let hobby: String
+    let favoriteCharacter: String
+    let location: String
+}
---
+++ week5/week5practice/week5practice/week5practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  week5practiceApp.swift
+//  week5practice
+//
+//  Created by 김미주 on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct week5practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            JSONParsingView()
+        }
+    }
+}
